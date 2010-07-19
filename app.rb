12c08afb6c1bf837d16bf46951373712658a481a
Simon Maddox <simon@simonmaddox.com>
2010-07-19 11:34:33 +0100

    adding mailer

--- app.rb
+++ app.rb
@@ -1,6 +1,21 @@
 require 'rubygems'
 require 'sinatra'
+require 'actionmailer'
 
 get '/' do
   "Hello"
+end
+
+post '/send/:to' do
+  raw = request.env["rack.input"].read
+  
+  ActionMailer::Base.smtp_settings = {
+    :address        => "smtp.sendgrid.net",
+    :port           => "25",
+    :authentication => :plain,
+    :user_name      => "simon@simonmaddox.com",
+    :password       => "mx463yp",
+    #:domain         => ENV['SENDGRID_DOMAIN'],
+  }
+  
 end
